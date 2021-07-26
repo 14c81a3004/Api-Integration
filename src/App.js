9943f274002ae32184608b39d50faf28d9657c67
14c81a3004 <sndpburri@gmail.com>
2021-07-26 13:13:45 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import * as React from "react";
+import {BrowserRouter as Router,Route,Link,Switch,Redirect, useParams, useHistory} from 'react-router-dom';
 import './App.css';
 
-function App() {
+function Apppage1() {
+  const [users, setUsers] = React.useState([]);
+  const demo = async () => {
+    const res = await fetch("https://reqres.in/api/users?page=1");
+    const json = await res.json();
+    setUsers(json.data);
+  };
+  React.useEffect(() => {
+    demo();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Api data page-1</h1>
+      <div className="flex">
+        {users.length &&
+          users.map((user) => {
+            return (
+                <div className='card' key={user.id}>
+                <p>{user.id}</p>
+                <p>
+                  <strong>{user.first_name} {user.last_name}</strong>
+                </p>
+                <p>{user.email}</p>
+                <img key={user.avatar} src={user.avatar} />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
-
-export default App;
+function Apppage2() {
+  const [users, setUsers] = React.useState([]);
+  const demo = async () => {
+    const res = await fetch("https://reqres.in/api/users?page=2");
+    const json = await res.json();
+    setUsers(json.data);
+  };
+  React.useEffect(() => {
+    demo();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Api data page-2</h1>
+      <div className="flex">
+        {users.length &&
+          users.map((user) => {
+            return (
+              <div className='card' key={user.id}>
+                <p>{user.id}</p>
+                <p>
+                  <strong>{user.first_name} {user.last_name}</strong>
+                </p>
+                <p>{user.email}</p>
+                <img key={user.avatar} src={user.avatar} />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+export default class ApiComponent extends React.Component{
+  render(){
+    return(
+      <>
+        <nav>
+        <Router>
+        <Switch>
+          <div className='row'>
+          <ul class="pagination">
+              <li class="page-item"><Link to='/page1' class="page-link" href="#"><button>1</button></Link></li>
+              <li class="page-item"><Link to='/page2' class="page-link" href="#"><button>2</button></Link></li>
+          </ul>
+          <Route exact path='/'>
+             <Apppage1 />
+          </Route>
+          <Route path='/page1'>
+             <Redirect to='/'></Redirect>
+          </Route>
+          <Route path='/page2'>
+              <Apppage2 />
+          </Route>              
+          </div>
+        </Switch>
+        </Router>
+      </nav>
+      </>
+    )
+  }
+}
